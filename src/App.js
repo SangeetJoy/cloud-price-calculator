0a88b35e48b071d9cafeae3a37e6cfb6c07319c7
Sangeet Joy <Sangeet.Joy@walmartlabs.com>
2021-06-20 02:23:30 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingPage from "./components/LandingPage/index"
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <LandingPage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./index.css"
+import Button from '@material-ui/core/Button';
+
+const rowData = [
+    {
+        id: 1,
+        mainText: "Thinking which cloud to choose",
+        subText: "Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.",
+        imageClassName: "image-container-1",
+        isButtonPresent: false
+    },
+    {
+        id: 2,
+        mainText: "Try our price calculator",
+        subText: "Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.",
+        imageClassName: "image-container-2",
+        isButtonPresent: true
+    }
+
+]
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            rowData.map(({ id, mainText, subText, imageClassName, isButtonPresent }) => {
+                return (
+                    <>
+                        <div className="row-container" style={{ backgroundColor: id % 2 === 0 && "#f6f6f6" }}>
+                            {id % 2 === 0 ?
+                                <>
+                                    <div className="text-container">
+                                        <h2 className="main-text">{mainText}</h2>
+                                        <p className="sub-text">{subText}</p>
+                                        {isButtonPresent &&
+                                            <div className="price-cal-button-container">
+                                                <Button variant="contained" color="primary" className="price-cal-button">
+                                                    Launch Calculator
+                                                </Button>
+                                            </div>}
+                                    </div>
+                                    <div className={imageClassName}></div>
+                                </> : <>
+                                    <div className={imageClassName}></div>
+                                    <div className="text-container">
+                                        <h2 className="main-text">{mainText}</h2>
+                                        <p className="sub-text">{subText}</p>
+                                        {isButtonPresent &&
+                                            <div className="price-cal-button-container">
+                                                <Button variant="contained" color="primary" className="price-cal-button">
+                                                    Launch Calculator
+                                                </Button>
+                                            </div>}
+                                    </div>
+                                </>
+                            }
+                        </div>
+                    </>
+                )
+            })
+        )
+    }
+}
+export default LandingPage;
